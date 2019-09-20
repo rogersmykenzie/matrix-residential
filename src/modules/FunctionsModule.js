4f188eb66108411c0916c4dc59a64cc2935cf5f0
mykenzie <mykenzierogers@gmail.com>
2019-09-20 00:13:53 -0500

    update

---
+++ src/modules/FunctionsModule.js
@@ -0,0 +1,16 @@
+module.exports = {
+    createWhenClicked: function(state, cb) {
+        return function(option) {
+            cb([...state, option])
+            console.log([...state, option])
+        }
+    },
+    createWhenUnclicked: function(state, cb) {
+        return function(option) {
+            let arr = [...state];
+            arr.splice(arr.indexOf(option), 1);
+            cb([...arr]);
+            console.log(arr);
+        }
+    }
+}
